2176aca812876f60abc5cafb0fecbf52e48c2ecd
swagatika99 <sswagatika99@gmail.com>
2020-04-16 08:07:33 +0530

    first commit

---
+++ Lab1/src/main/java/com/cg/iter/spring/4th/EmployeeService.java
@@ -0,0 +1,5 @@
+package com.cg.iter.demo1.4th;
+
+public interface EmployeeService {
+public Employee getDetails(int empId);
+}
---
+++ Lab1/src/main/java/com/cg/iter/spring/4th/EmployeeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cg.iter.demo1.4th;
+
+public class EmployeeServiceImpl implements EmployeeService{
+
+	public Employee getDetails(int empId) {
+		EmployeeDAO emp=new EmployeeDAO();
+		return emp.getDetails(empId);
+	}
+ 
+}
